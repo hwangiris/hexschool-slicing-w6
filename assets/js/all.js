dbed0ab4e6d39e03685dbb012cb1786afa42970c
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-08-14 13:39:27 +0800

    Update 2022-08-14T05:39:22.821Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,14 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  var swiperTestimonial = new Swiper(".swiper--testimonial", {
+    navigation: {
+      nextEl: ".function__next",
+      prevEl: ".function__prev"
+    },
+    pagination: {
+      el: ".function__pagination"
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
